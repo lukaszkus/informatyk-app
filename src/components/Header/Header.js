3da04b4ff44433a4b142576202823638341ec893
lukaszkus <kusiu@vp.pl>
2021-02-05 14:19:55 +0100

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import './Header.scss'
 
+import { ReactComponent as Janush } from '../../images/janush.svg'
+
 export default function Header() {
     return (
         <header id="header" className="header-wrapper">
             <div className="container">
                 <div className="header">
-                    Header
+                    <div className="header-heading">
+                        <h1>Potrzebujesz stworzyć
+                        stronę internetową
+                        lub aplikację
+                        i nie wiesz gdzie
+                        szukać pomocy?
+                            </h1>
+                        <ul>
+                            <li>Skorzystaj ze wsparcia sprawdzonych profesjonalistów IT.</li>
+                            <li>Określ, w jakich terminach i godzinach potrzebujesz wsparcia przy realizacji projektu.</li>
+                        </ul>
+                        <div className="header-cta">
+                            <a href="#qqq">Dowiedz się jak to działa</a>
+                        </div>
+                    </div>
+                    <div className="header-image">
+                        <Janush className="janush" alt="Janush the programmer" />
+                    </div>
                 </div>
             </div>
         </header>
