314485ab331d73dff3f3eec0eb3283707cc9d876
lukaszkus <kusiu@vp.pl>
2021-02-05 18:34:14 +0100

    header upgrade

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,43 @@
 import React from 'react'
+import { Link } from "react-scroll";
 import './Header.scss'
 
 import { ReactComponent as Janush } from '../../images/janush.svg'
 
 export default function Header() {
-    return (
-        <header id="header" className="header-wrapper">
-            <div className="container">
-                <div className="header">
-                    <div className="header-heading">
-                        <h1>Potrzebujesz stworzyć
-                        stronę internetową
-                        lub aplikację
-                        i nie wiesz gdzie
-                        szukać pomocy?
+  return (
+    <header id="header" className="header-wrapper">
+      <div className="container">
+        <div className="header">
+          <div className="header-heading">
+            <h1>Potrzebujesz stworzyć
+            stronę internetową
+            lub aplikację
+            i nie wiesz gdzie
+            szukać pomocy?
                             </h1>
-                        <ul>
-                            <li>Skorzystaj ze wsparcia sprawdzonych profesjonalistów IT.</li>
-                            <li>Określ, w jakich terminach i godzinach potrzebujesz wsparcia przy realizacji projektu.</li>
-                        </ul>
-                        <div className="header-cta">
-                            <a href="#qqq">Dowiedz się jak to działa</a>
-                        </div>
-                    </div>
-                    <div className="header-image">
-                        <Janush className="janush" alt="Janush the programmer" />
-                    </div>
-                </div>
+            <ul>
+              <li>Skorzystaj ze wsparcia sprawdzonych profesjonalistów IT.</li>
+              <li>Określ, w jakich terminach i godzinach potrzebujesz wsparcia przy realizacji projektu.</li>
+            </ul>
+            <div className="header-cta">
+              {/* <a href="#qqq">Dowiedz się jak to działa</a> */}
+              <Link
+                to="howItWorks"
+                spy={true}
+                smooth={true}
+                offset={-80}
+                duration={500}
+              >
+                Dowiedz się jak to działa
+              </Link>
             </div>
-        </header>
-    )
+          </div>
+          <div className="header-image">
+            <Janush className="janush" alt="Janush the programmer" />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
