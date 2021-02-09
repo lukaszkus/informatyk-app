506160cc677f6a628d706cec70d5a54a468c3eda
lukaszkus <kusiu@vp.pl>
2021-02-09 07:54:14 +0100

    ScrollTopBtn update

--- src/components/Navigation/ScrollTopBtn.js
+++ src/components/Navigation/ScrollTopBtn.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { FiArrowUp } from 'react-icons/fi';
 import { animateScroll as scroll } from "react-scroll"
 
@@ -7,14 +7,29 @@ import './ScrollTopBtn.scss'
 export default function ScrollTopBtn() {
     const [isVisible, setIsVisible] = useState(false)
 
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      };
+
     const scrollToTop = () => {
         scroll.scrollToTop();
       };
 
+      useEffect(() => {
+        window.addEventListener("scroll", toggleVisibility);
+      }, []);
+
 
     return (
+        <>
+        {isVisible && 
         <div className="scrollTop-btn" onClick={scrollToTop}>
             <FiArrowUp/>
-        </div>
+        </div>}
+        </>
     )
 }
